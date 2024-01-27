3e71855c232f56f51e6fab9be7067aa1e15e112f
manikOnline <thmanik1241@gmail.com>
2024-01-27 22:08:49 +0600

    make responsive

--- src/components/productCarousel/ProductCarousel.tsx
+++ src/components/productCarousel/ProductCarousel.tsx
@@ -14,7 +14,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // Import Swiper React components
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
@@ -24,11 +24,36 @@ import data from "../../../public/data.json";
 import "./productCarousel.css";
 const SuggestedProducts: React.FC = () => {
   const [value, setValue] = useState<string>("tab1");
+  const [slidesPerView, setSlidesPerView] = useState(1);
 
   const handleChange = (newValue: string) => {
     setValue(newValue);
     // You can add additional logic here if needed
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Update slidesPerView based on screen width
+      if (window.innerWidth < 200) {
+        setSlidesPerView(1); // Small screens
+      } else if (window.innerWidth < 600) {
+        setSlidesPerView(2);
+      } else {
+        setSlidesPerView(6); // Medium and larger screens
+      }
+    };
+
+    // Initial setup
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="mt-8 bg-gray-100 rounded" style={{ width: "100%" }}>
@@ -75,7 +100,7 @@ const SuggestedProducts: React.FC = () => {
           <TabPanel value="tab1">
             <div>
               <Swiper
-                slidesPerView={6}
+                slidesPerView={slidesPerView}
                 spaceBetween={2}
                 pagination={{
                   clickable: true,
@@ -123,7 +148,7 @@ const SuggestedProducts: React.FC = () => {
           <TabPanel value="tab2">
             <div>
               <Swiper
-                slidesPerView={6}
+                slidesPerView={slidesPerView}
                 spaceBetween={2}
                 pagination={{
                   clickable: true,
@@ -171,7 +196,7 @@ const SuggestedProducts: React.FC = () => {
           <TabPanel value="tab3">
             <div>
               <Swiper
-                slidesPerView={6}
+                slidesPerView={slidesPerView}
                 spaceBetween={2}
                 pagination={{
                   clickable: true,
