c67aeb39159722db409fb7c06d29ec44646e4c34
Md Tofazzol Hossain Manik <you@example.com>
2024-06-23 01:39:36 +0600

    footer

--- src/app/(with-layout)/shop/loading.tsx
+++ src/app/(with-layout)/shop/loading.tsx
@@ -1,9 +1,25 @@
-const Loading = () => {
+import Image from "next/image";
+
+const loading = () => {
   return (
-    <div>
-      <h2 className="text-7xl">loading....</h2>
+    <div className="flex flex-col items-center justify-center gap-5 h-[100vh]">
+      <Image
+        src="/images/logo/logo.png"
+        alt="logo"
+        width={100}
+        height={100}
+        priority={true}
+      ></Image>
+      <h1 className="text-4xl">Loading... Please wait</h1>
+      <Image
+        src="/images/animations/loading.gif"
+        alt="loading"
+        width={100}
+        height={100}
+        priority={true}
+      ></Image>
     </div>
   );
 };
 
-export default Loading;
+export default loading;
--- src/components/ui/shared/footer/components/MainFooterContent.tsx
+++ src/components/ui/shared/footer/components/MainFooterContent.tsx
@@ -1,4 +1,118 @@
-// ! Animation-01
+// ! footer-1
+import ContainerMax from "@/components/containerMax/ContainerMax";
+import Image from "next/image";
+import Link from "next/link";
+import { BiSupport } from "react-icons/bi";
+
+const quickLinks = [
+  {
+    name: "Products",
+    url: "/shop",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Store location",
+    url: "/store-location",
+  },
+  {
+    name: "Refund policy",
+    url: "/refund-policy",
+  },
+];
+
+const socialLinks = [
+  {
+    icon: "/images/social_icons/facebook.png",
+    url: "https://www.facebook.com/oneselfbd?mibextid=ZbWKwL",
+  },
+  {
+    icon: "/images/social_icons/whatsapp.png",
+    url: "https://api.whatsapp.com/send?phone=+8801967214215&text=Is%20anyone%20available?",
+  },
+  {
+    icon: "/images/social_icons/groups.png",
+    url: "https://www.facebook.com/groups/1246098009387414/?ref=share&mibextid=adzO7l&paipv=0&eav=AfZUgPMBGmrQ-r6t7oD9pFOoIp28bG6IiWtZxAAnmkGDRyF_TDU_jjII4g3pMojA1dI&_rdr",
+  },
+];
+
+const MainFooterContent = () => {
+  return (
+    <div className="relative pb-10 pt-10 text-white">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_90%_90%_at_50%_-30%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <ContainerMax>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
+          <div className="md:col-span-2 space-y-5">
+            <Image
+              src="/images/logo/logo.png"
+              alt="Company logo"
+              width={400}
+              height={200}
+              className="w-32 md:w-44 rounded-md shadow-md"
+              priority
+            />
+            <div className="flex gap-4 items-center">
+              <BiSupport className="w-10 h-10 md:w-12 md:h-12 text-secondary" />
+              <div>
+                <p className="text-sm">Have a question? Call us right now</p>
+                <p className="text-lg md:text-xl font-semibold">
+                  +8801967214215
+                </p>
+              </div>
+            </div>
+            <address className="font-semibold not-italic space-y-1">
+              <p className="text-lg">Address</p>
+              <p className="text-base">Khulna, Bangladesh</p>
+              {/* TODO: Change the address */}
+            </address>
+          </div>
+          <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-10">
+            <div>
+              <h2 className="font-bold mb-4 text-secondary text-xl">
+                Quick links
+              </h2>
+              <div className="space-y-2">
+                {quickLinks.map((item, index) => (
+                  <Link
+                    key={index}
+                    href={item.url}
+                    className="block text-base hover:text-secondary transition-all font-semibold"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+            <div>
+              <h2 className="font-bold mb-4 text-secondary text-xl">
+                Social links
+              </h2>
+              <div className="flex gap-4">
+                {socialLinks.map((item, index) => (
+                  <Link href={item.url} key={index} target="_blank">
+                    <Image
+                      src={item.icon}
+                      alt="Social icon"
+                      width={100}
+                      height={100}
+                      className="w-10 h-10 md:w-12 md:h-12 hover:scale-110 transition-transform"
+                    />
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </ContainerMax>
+    </div>
+  );
+};
+
+export default MainFooterContent;
+
+// ! Animation footer-02
 
 // import ContainerMax from "@/components/containerMax/ContainerMax";
 // import Image from "next/image";
@@ -43,228 +157,6 @@
 //       <div className="absolute inset-0 z-0 flex justify-center items-center bg-black/20 overflow-hidden">
 //         <div className="absolute w-[150%] h-[150%] bg-[radial-gradient(circle,rgba(255,255,255,0)20%,rgba(255,255,255,0.1)21%)] bg-[size:80px_80px] animate-spin-slow"></div>
 //       </div>
-//       <ContainerMax>
-//         <div className="grid grid-cols-1 md:grid-cols-4">
-//           <div className="md:col-span-2 space-y-3">
-//             <Image
-//               src="/images/logo/logo.png"
-//               alt="Company logo"
-//               width={400}
-//               height={200}
-//               className="w-32 md:w-44 rounded-md shadow-md"
-//               priority
-//             />
-//             <div className="flex gap-2 items-center">
-//               <BiSupport className="w-8 h-8 md:w-12 md:h-12" />
-//               <div>
-//                 <p className="text-xs">Have a question? Call us right now</p>
-//                 <p className="text-base md:text-xl font-semibold">
-//                   +8801967214215
-//                 </p>
-//               </div>
-//             </div>
-//             <address className="font-semibold">
-//               <p>Address</p>
-//               <p>Khulna, bangladesh</p>
-//               {/* TODO: Change the address */}
-//             </address>
-//           </div>
-//           <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 pt-5 gap-5">
-//             <div>
-//               <h2 className="font-bold mb-2 text-secondary">Quick links</h2>
-//               <div className="space-y-1">
-//                 {quickLinks.map((item, index) => (
-//                   <Link
-//                     key={index}
-//                     href={item.url}
-//                     className="block text-sm hover:text-secondary transition-all font-semibold w-max"
-//                   >
-//                     {item.name}
-//                   </Link>
-//                 ))}
-//               </div>
-//             </div>
-//             <div>
-//               <h2 className="font-bold mb-2 text-secondary">Social links</h2>
-//               <div className="flex gap-2">
-//                 {socialLinks.map((item, index) => (
-//                   <Link href={item.url} key={index} target="_blank">
-//                     <Image
-//                       src={item.icon}
-//                       alt="Social icon"
-//                       width={100}
-//                       height={100}
-//                       className="w-10 h-10 hover:scale-105 transition-all"
-//                     />
-//                   </Link>
-//                 ))}
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </ContainerMax>
-//     </div>
-//   );
-// };
-
-// export default MainFooterContent;
-
-// ! animation-2
-
-import ContainerMax from "@/components/containerMax/ContainerMax";
-import Image from "next/image";
-import Link from "next/link";
-import { BiSupport } from "react-icons/bi";
-
-const quickLinks = [
-  {
-    name: "Products",
-    url: "/shop",
-  },
-  {
-    name: "About",
-    url: "/about",
-  },
-  {
-    name: "Store location",
-    url: "/store-location",
-  },
-  {
-    name: "Refund policy",
-    url: "/refund-policy",
-  },
-];
-
-const socialLinks = [
-  {
-    icon: "/images/social_icons/facebook.png",
-    url: "https://www.facebook.com/oneselfbd?mibextid=ZbWKwL",
-  },
-  {
-    icon: "/images/social_icons/whatsapp.png",
-    url: "https://api.whatsapp.com/send?phone=+8801967214215&text=Is%20anyone%20available?",
-  },
-  {
-    icon: "/images/social_icons/groups.png",
-    url: "https://www.facebook.com/groups/1246098009387414/?ref=share&mibextid=adzO7l&paipv=0&eav=AfZUgPMBGmrQ-r6t7oD9pFOoIp28bG6IiWtZxAAnmkGDRyF_TDU_jjII4g3pMojA1dI&_rdr",
-  },
-];
-
-const MainFooterContent = () => {
-  return (
-    <div className="pb-5 pt-6 relative bg-gray-900 text-white">
-      {/* Animated background */}
-      <div className="absolute inset-0 z-0 flex justify-center items-center bg-black/20 overflow-hidden">
-        <div className="absolute w-[150%] h-[150%] bg-[radial-gradient(circle,rgba(255,255,255,0)20%,rgba(255,255,255,0.1)21%)] bg-[size:80px_80px] animate-spin-slow"></div>
-      </div>
-      {/* Content container */}
-      <ContainerMax>
-        <div className="grid grid-cols-1 md:grid-cols-4">
-          <div className="md:col-span-2 space-y-3">
-            <Image
-              src="/images/logo/logo.png"
-              alt="Company logo"
-              width={400}
-              height={200}
-              className="w-32 md:w-44 rounded-md shadow-md"
-              priority
-            />
-            <div className="flex gap-2 items-center">
-              <BiSupport className="w-8 h-8 md:w-12 md:h-12 text-white" />
-              <div>
-                <p className="text-xs">Have a question? Call us right now</p>
-                <p className="text-base md:text-xl font-semibold">
-                  +8801967214215
-                </p>
-              </div>
-            </div>
-            <address className="font-semibold">
-              <p>Address</p>
-              <p>Khulna, Bangladesh</p>
-              {/* TODO: Change the address */}
-            </address>
-          </div>
-          <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 pt-5 gap-5">
-            <div>
-              <h2 className="font-bold mb-2 text-secondary">Quick links</h2>
-              <div className="space-y-1">
-                {quickLinks.map((item, index) => (
-                  <Link
-                    key={index}
-                    href={item.url}
-                    className="block text-sm hover:text-secondary transition-all font-semibold w-max"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-            <div>
-              <h2 className="font-bold mb-2 text-secondary">Social links</h2>
-              <div className="flex gap-2">
-                {socialLinks.map((item, index) => (
-                  <Link href={item.url} key={index} target="_blank">
-                    <Image
-                      src={item.icon}
-                      alt="Social icon"
-                      width={100}
-                      height={100}
-                      className="w-10 h-10 hover:scale-105 transition-all"
-                    />
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </ContainerMax>
-    </div>
-  );
-};
-
-export default MainFooterContent;
-
-// import ContainerMax from "@/components/containerMax/ContainerMax";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { BiSupport } from "react-icons/bi";
-// const quickLinks = [
-//   {
-//     name: "Products",
-//     url: "/shop",
-//   },
-//   {
-//     name: "About",
-//     url: "/about",
-//   },
-//   {
-//     name: "Store location",
-//     url: "/store-location",
-//   },
-//   {
-//     name: "Refund policy",
-//     url: "/refund-policy",
-//   },
-// ];
-
-// const socialLinks = [
-//   {
-//     icon: "/images/social_icons/facebook.png",
-//     url: "https://www.facebook.com/oneselfbd?mibextid=ZbWKwL",
-//   },
-//   {
-//     icon: "/images/social_icons/whatsapp.png",
-//     url: "https://api.whatsapp.com/send?phone=+8801967214215&text=Is%20anyone%20available?",
-//   },
-//   {
-//     icon: "/images/social_icons/groups.png",
-//     url: "https://www.facebook.com/groups/1246098009387414/?ref=share&mibextid=adzO7l&paipv=0&eav=AfZUgPMBGmrQ-r6t7oD9pFOoIp28bG6IiWtZxAAnmkGDRyF_TDU_jjII4g3pMojA1dI&_rdr",
-//   },
-// ];
-// const MainFooterContent = () => {
-//   return (
-//     <div className="pb-5 pt-6 relative ">
-//       <div className="absolute inset-0 -z-10 h-full w-full bg-base-100 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
 //       <ContainerMax>
 //         <div className="grid grid-cols-1 md:grid-cols-4">
 //           <div className="md:col-span-2 space-y-3">
