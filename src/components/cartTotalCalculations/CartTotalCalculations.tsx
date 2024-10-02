d0968f0abd90b9b4df048aaf0f282f8220849916
Md Tofazzol Hossain Manik <you@example.com>
2024-10-03 03:13:22 +0600

    coupon apply and showing discount amount

--- src/components/cartTotalCalculations/CartTotalCalculations.tsx
+++ src/components/cartTotalCalculations/CartTotalCalculations.tsx
@@ -1,8 +1,6 @@
-"use client";
 import { TRootState } from "@/types/rootState";
 import TShippingCharges from "@/types/shippingCharge";
 import { useSelector } from "react-redux";
-
 import SelectShippingMethod from "../selectShippingMethod/SelectShippingMethod";
 import { Skeleton } from "../ui/skeleton";
 
@@ -22,16 +20,15 @@ const CartTotalCalculations = ({
   const couponInfo = useSelector((state: TRootState) => state.coupon);
 
   const discount = (couponInfo.percentage / 100) * (totalCost ? totalCost : 0);
-
   const applicableDiscount =
     discount > couponInfo.maxDiscountAmount
       ? couponInfo.maxDiscountAmount
       : discount;
 
-  // Calculate ultimateTotalCost
+  // Calculate ultimateTotalCost (with discount) and totalWithShipping
+  const totalWithoutCoupon =
+    (totalCost ? totalCost : 0) + (selectedShipping.amount || 0);
   const ultimateTotalCost = (totalCost ? totalCost : 0) - applicableDiscount;
-
-  // Add shipping amount if there is a selected shipping method
   const totalWithShipping = ultimateTotalCost + (selectedShipping.amount || 0);
 
   return (
@@ -40,30 +37,125 @@ const CartTotalCalculations = ({
         <span className="font-bold">Subtotal</span>
         <span className="text-sm font-semibold">
           {costLoading ? (
-            <>
-              <Skeleton className="h-4 w-[100px]" />
-            </>
+            <Skeleton className="h-4 w-[100px]" />
           ) : (
             <>{totalCost} &#2547;</>
           )}
         </span>
       </div>
+
+      {/* Shipping Method Selection */}
       <SelectShippingMethod shippingCharges={shippingCharges} />
 
+      {/* total amout */}
       <div className="flex justify-between py-3 px-2">
         <p className="font-bold">Total</p>
-        <div className="font-bold">
+        <span className="font-bold">
           {costLoading ? (
-            <>
-              <Skeleton className="h-4 w-[100px]" />
-            </>
+            <Skeleton className="h-4 w-[100px]" />
           ) : (
-            <>{totalWithShipping} &#2547;</>
+            <>{totalWithoutCoupon} &#2547;</>
           )}
-        </div>
+        </span>
       </div>
+      {/* Discount Section */}
+      {applicableDiscount > 0 && (
+        <div className="flex justify-between pt-3 pb-1  px-2">
+          <p className="font-bold text-primary">Coupon Discount</p>
+          <span className="text-sm font-semibold text-primary">
+            - {applicableDiscount} &#2547;
+          </span>
+        </div>
+      )}
+
+      {/* Grand Total Section */}
+      {applicableDiscount > 0 && (
+        <div className="">
+          <hr className="py-1 border-t-2 border-gray-200 w-full" />
+          <div className="flex justify-between pb-3 pt-1 px-2">
+            <p className="font-bold">Grand Total (with coupon)</p>
+            <span className="font-bold">
+              {costLoading ? (
+                <Skeleton className="h-4 w-[100px]" />
+              ) : (
+                <>{totalWithShipping} &#2547;</>
+              )}
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default CartTotalCalculations;
+
+// "use client";
+// import { TRootState } from "@/types/rootState";
+// import TShippingCharges from "@/types/shippingCharge";
+// import { useSelector } from "react-redux";
+
+// import SelectShippingMethod from "../selectShippingMethod/SelectShippingMethod";
+// import { Skeleton } from "../ui/skeleton";
+
+// const CartTotalCalculations = ({
+//   shippingCharges,
+//   totalCost,
+//   costLoading = false,
+// }: {
+//   shippingCharges?: TShippingCharges[];
+//   totalCost?: number;
+//   costLoading?: boolean;
+// }) => {
+//   const selectedShipping = useSelector(
+//     (state: TRootState) => state.shippingClass
+//   );
+
+//   const couponInfo = useSelector((state: TRootState) => state.coupon);
+
+//   const discount = (couponInfo.percentage / 100) * (totalCost ? totalCost : 0);
+
+//   const applicableDiscount =
+//     discount > couponInfo.maxDiscountAmount
+//       ? couponInfo.maxDiscountAmount
+//       : discount;
+
+//   // Calculate ultimateTotalCost
+//   const ultimateTotalCost = (totalCost ? totalCost : 0) - applicableDiscount;
+
+//   // Add shipping amount if there is a selected shipping method
+//   const totalWithShipping = ultimateTotalCost + (selectedShipping.amount || 0);
+
+//   return (
+//     <div>
+//       <div className="flex gap-2 justify-between items-center py-5 px-2 border-b-2">
+//         <span className="font-bold">Subtotal</span>
+//         <span className="text-sm font-semibold">
+//           {costLoading ? (
+//             <>
+//               <Skeleton className="h-4 w-[100px]" />
+//             </>
+//           ) : (
+//             <>{totalCost} &#2547;</>
+//           )}
+//         </span>
+//       </div>
+//       <SelectShippingMethod shippingCharges={shippingCharges} />
+
+//       <div className="flex justify-between py-3 px-2">
+//         <p className="font-bold">Total</p>
+//         <div className="font-bold">
+//           {costLoading ? (
+//             <>
+//               <Skeleton className="h-4 w-[100px]" />
+//             </>
+//           ) : (
+//             <>{totalWithShipping} &#2547;</>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CartTotalCalculations;
