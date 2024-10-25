e59ef57027b82760a240378b6a7b25bc6a7f972f
mustaqimbd <mustaqimkhanbd@gmail.com>
2024-10-25 17:30:40 +0600

    [Update] Navbar and Footer, fix error on details page

--- src/components/ui/shared/header/menuBar/MenuBar.tsx
+++ src/components/ui/shared/header/menuBar/MenuBar.tsx
@@ -63,7 +63,7 @@ const MenuBar = ({
                 <MenubarTrigger>
                   <Link
                     href={menuItem.href}
-                    className={`text-md md:text-sm text-white hover:text-secondary flex justify-between items-center text-lg font-semibold py-2 px-4 transition duration-200 ${isActive ? " text-white" : ""} ${visibilityClass}`}
+                    className={`text-sm md:text-md  lg:text-lg hover:!text-secondary flex justify-between items-center font-semibold py-2 px-4 transition duration-200 ${isActive ? "!text-secondary" : "!text-white"} ${visibilityClass}`}
                     onClick={onMenuItemClick} // Close sidebar on link click
                   >
                     {menuItem.title}
@@ -75,7 +75,9 @@ const MenuBar = ({
           } else {
             return (
               <MenubarMenu key={menuItem.href}>
-                <MenubarTrigger className="text-white ps-8 pt-3 md:pt-2 md:ps-0 font-semibold">
+                <MenubarTrigger
+                  className={`ps-8 pt-3 md:pt-2 md:ps-0 font-semibold  md:text-md lg:text-lg md:mx-auto hover:cursor-pointer hover:!text-secondary transition duration-200 ${isActive ? "!text-secondary" : "!text-white"}`}
+                >
                   {menuItem.title} <IoMdArrowDropdown />
                 </MenubarTrigger>
                 <MenubarContent className="-mt-3">
