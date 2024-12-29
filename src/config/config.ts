524ffdc3016c0041028ce6f772bc8866b57e4fd9
Md Tofazzol Hossain Manik <you@example.com>
2024-12-29 13:18:21 +0600

    config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  base_url: process.env.NEXT_PUBLIC_BASE_URL,
+  base_url: " https://electrocommerce.mustaqim.site ",
   facebook_pixel: process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID,
 };
 
